2a35d065ad8817c42b294d4be8947a71ed3a7fd6
ala_747 <nfanti@hotmail.com>
2011-08-28 20:33:19 +0200

    A little code cleanup and repositioning after window resize

--- js/jquery-earsnav.js
+++ js/jquery-earsnav.js
@@ -25,7 +25,7 @@
 
 		var settings = $.extend(true, {}, defaults, options);
 
-		return this.each(function() {
+		return this.each(function() { // Main
 			
 			var el = new Array();
 			if(settings.type !== null && settings.type == 'next'){ // is it a single "next"? let's fake the "prev"
@@ -42,12 +42,137 @@
 			$.each(el, function (i, e) {
 				if(typeof e !== 'undefined'){
 					var side = (i === 0)?'left':'right';
-					drawIt($(this).html(), $(this).attr('href'), side);
-					$(this).hide();
+					drawIt(this.html(), this.attr('href'), side);
+					this.hide();
 				}
 			});
 		});
 
+		function drawIt (title, href, side) {
+			// Elements Styles
+			var wrapperStyle = {
+				background: settings.backgroundColor,
+				color: settings.fontColor,
+				fontFamily: settings.fontFamily,
+				fontSize: settings.fontSize,
+				letterSpacing: settings.letterSpacing,
+				textTransform: settings.textTransform,
+				textDecoration: 'none',
+				opacity: settings.opacity,
+				position: 'fixed',
+				display: 'block',
+				width: settings.minWidth,
+				height: settings.fontSize,
+				padding: '60px 10px',
+				lineHeight: '1',
+				overflow: 'visible',
+				'zoom': 1,
+				zIndex: 10000
+			};
+
+			var pointerStyle = {
+				borderWidth: settings.pointerSize,
+				borderStyle: 'solid',
+				borderColor: ((side == 'right')?'transparent transparent transparent '+ settings.backgroundColor:'transparent '+ settings.backgroundColor +' transparent transparent'),
+				display: 'block',
+				position: 'fixed',
+				'zoom': 1
+			};
+
+			var defaultTitleStyle = {};
+
+			var articleTitleStyle = {
+				position: 'absolute'
+			};
+
+			// Elements
+			var $wrapper = $('<a />')
+				.attr('id', 'earsnav-'+ side)
+				.addClass('earsnav')
+				.css(wrapperStyle)
+				.css(settings.wrapperCSS) // Set Extra CSS!
+				.attr('href', href);
+
+			var $defaultTitle = $('<span />')
+				.addClass('earsnav-default-title')
+				.css(defaultTitleStyle)
+				.html(((side == 'right')?settings.titleNext:settings.titlePrev));
+
+			var $articleTitle = $('<span />')
+				.addClass('earsnav-article-title')
+				.css( articleTitleStyle )
+				.css({top: $($wrapper).outerHeight(true) / 2})
+				.html( title );
+
+			if(settings.pointerSize > 0){ // is the pointer size defined?
+				var $pointer = $('<strong />')
+					.css(pointerStyle);
+				if(side == 'right')
+					$pointer.css({right:-settings.pointerSize});
+				else
+					$pointer.css({left:-settings.pointerSize});
+	
+				$pointer.css({
+					top : $(window).height() / 2 - $($pointer).outerHeight(true) / 2
+				});
+				$pointer.appendTo($wrapper);
+			}
+
+			// Extra Styles
+			if(settings.borderRadius > 0) // Rounded Corners?
+				$wrapper.css({
+					'-moz-border-radius': settings.borderRadius +'px',
+					'-webkit-border-radius': settings.borderRadius +'px',
+					'border-radius': settings.borderRadius +'px'
+				});
+
+			if(settings.shadow === true) // Drop Shadow Perhaps?
+				$wrapper.css({
+					'-moz-box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
+					'-webkit-box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
+					'box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
+				});
+
+			// Side Specific Styles
+			if(side == 'right'){
+				$wrapper.css({
+					right: settings.pointerSize,
+					textAlign: 'right'
+				});
+				$articleTitle.css({right: 10});
+			} else {
+				$wrapper.css({
+					left: settings.pointerSize
+				});
+				$articleTitle.css({
+					left: 10
+				});
+			}
+
+			$wrapper.css({ // Needed AFTER the Element was created :(
+				top : $(window).height() / 2 - $($wrapper).outerHeight(true) / 2
+			});
+
+			$defaultTitle.appendTo($wrapper);
+			$articleTitle.appendTo($wrapper);
+			$wrapper.appendTo(settings.container);
+
+			$('.earsnav-article-title').hide();
+			$($wrapper).hover(onHover, onOut);
+
+			// Is anybody resizing the browser window?? Let's repositionate stuff ASAP
+			$(window).bind('resize', function () {
+				if($pointer)
+					$pointer.css({
+						top : $(window).height() / 2 - $($pointer).outerHeight(true) / 2
+					});
+				$wrapper.css({
+					top : $(window).height() / 2 - $($wrapper).outerHeight(true) / 2
+				});
+			});
+		}
+
+		// Effects
 		function onHover() {
 			$(this).children('.earsnav-default-title').animate({
 				opacity: 'hide'
@@ -67,7 +192,6 @@
 					});
 				});
 			});
-
 			return false;
 		}
 		function onOut() {
@@ -85,111 +209,7 @@
 					$(this).children('.earsnav-default-title').animate({opacity: 'show'}, 100);
 				});
 			});
-
 			return false;
-		}
-
-		function drawIt (title, href, side) {
-			
-			var $style = {
-				background: settings.backgroundColor,
-				color: settings.fontColor,
-				fontFamily: settings.fontFamily,
-				fontSize: settings.fontSize,
-				letterSpacing: settings.letterSpacing,
-				textTransform: settings.textTransform,
-				textDecoration: 'none',
-				opacity: settings.opacity,
-				position: 'fixed',
-				display: 'block',
-				width: settings.minWidth,
-				height: settings.fontSize,
-				padding: '60px 10px',
-				lineHeight: '1',
-				overflow: 'visible',
-				'zoom': 1,
-				zIndex: 10000
-			};
-			
-			var $wrapper = $('<a />')
-				.attr('id', 'earsnav-'+ side)
-				.addClass('earsnav')
-				.css($style)
-				.css(settings.wrapperCSS) // ** Set CSS!
-				.attr('href', href);
-			var wrapperHeight = $($wrapper).outerHeight(true);  
-			var windowHeight = $(window).height();
-			
-			var $defaultTitle = $('<span />')
-				.addClass('earsnav-default-title')
-				.css({})
-				.html(((side == 'right')?settings.titleNext:settings.titlePrev));
-			var $articleTitle = $('<span />')
-				.addClass('earsnav-article-title')
-				.css({
-					top : wrapperHeight / 2 +'px',
-					position: 'absolute'
-				})
-				.html( title );
-			if(side == 'right')
-					$articleTitle.css({right: 10});
-				else
-					$articleTitle.css({left: 10});
-
-			if(settings.pointerSize > 0){
-				var $pointer = $('<strong />')
-					.css({
-						borderWidth: settings.pointerSize,
-						borderStyle: 'solid',
-						borderColor: ((side == 'right')?'transparent transparent transparent '+ settings.backgroundColor:'transparent '+ settings.backgroundColor +' transparent transparent'),
-						display: 'block',
-						position: 'fixed',
-						'zoom': 1,
-						//top: windowHeight / 2 - $($pointer).outerHeight() / 2,
-					});
-				if(side == 'right')
-					$pointer.css({right:-settings.pointerSize});
-				else
-					$pointer.css({left:-settings.pointerSize});
-	
-				$pointer.css({
-					"top" : windowHeight / 2 - $($pointer).outerHeight(true) / 2
-				});
-					//.html(((side == 'right')?'&#9654;':'&#9664;'));
-				$pointer.appendTo($wrapper);
-			}
-
-			if(settings.borderRadius > 0)
-				$wrapper.css({
-					'-moz-border-radius': settings.borderRadius +'px',
-					'-webkit-border-radius': settings.borderRadius +'px',
-					'border-radius': settings.borderRadius +'px'
-				});
-			if(settings.shadow === true)
-				$wrapper.css({
-					'-moz-box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
-					'-webkit-box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
-					'box-shadow': '0 2px 6px rgba(50, 50, 50, .5)',
-				});
-			if(side == 'right')
-				$wrapper.css({
-					right: settings.pointerSize,
-					textAlign: 'right'
-				});
-			else
-				$wrapper.css({left:settings.pointerSize});
-			$wrapper.css({
-				"top" : windowHeight / 2 - wrapperHeight / 2
-			}
-				
-			);
-
-			$defaultTitle.appendTo($wrapper);
-			$articleTitle.appendTo($wrapper);
-			$wrapper.appendTo(settings.container);
-			
-			$('.earsnav-article-title').hide();
-			$($wrapper).hover(onHover, onOut);
 		}
 	}
 })(jQuery);
